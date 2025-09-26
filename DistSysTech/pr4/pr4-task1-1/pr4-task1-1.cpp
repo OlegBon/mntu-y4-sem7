ca098f1ab0aebca70b0fc50a2bb8d0f845499458
OlegBon <oleh.bon@yahoo.com>
2025-09-26 12:24:27 +0300

    add pr4-task1-1 for WebDev

---
+++ DistSysTech/pr4/pr4-task1-1/pr4-task1-1.cpp
@@ -0,0 +1,13 @@
+﻿#include <omp.h>
+#include <stdio.h>
+
+int main()
+{
+	//omp_set_num_threads(6); // Задаємо кількість потоків у паралельному регіоні
+
+#pragma omp parallel
+    {
+        printf("The parallel region is executed by thread %d\n", omp_get_thread_num());
+    }
+    return 0;
+}
