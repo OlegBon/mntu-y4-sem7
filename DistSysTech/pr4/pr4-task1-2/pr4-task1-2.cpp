c68d6b18a3bb55ca7eb344a647b4b1ed1d427877
OlegBon <oleh.bon@yahoo.com>
2025-09-26 12:32:31 +0300

    add pr4-task1-2 for WebDev

---
+++ DistSysTech/pr4/pr4-task1-2/pr4-task1-2.cpp
@@ -0,0 +1,11 @@
+﻿#include <omp.h>
+#include <stdio.h>
+
+int main()
+{
+#pragma omp parallel num_threads(6)
+    {
+        printf("Thread %d is working\n", omp_get_thread_num());
+    }
+return 0;
+}
