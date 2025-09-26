72b5340e2d224b7030e926edf679439e673ba046
OlegBon <oleh.bon@yahoo.com>
2025-09-26 12:37:14 +0300

    add pr4-task1-3 for WebDev

---
+++ DistSysTech/pr4/pr4-task1-3/pr4-task1-3.cpp
@@ -0,0 +1,14 @@
+﻿#include <omp.h>
+#include <stdio.h>
+
+int main()
+{
+    omp_set_dynamic(0); // Вимкнути динамічну зміну кількості потоків
+    omp_set_num_threads(12); // Встановити глобальну кількість потоків
+
+#pragma omp parallel num_threads(6)
+    {
+        printf("Thread %d is active\n", omp_get_thread_num());
+    }
+    return 0;
+}
