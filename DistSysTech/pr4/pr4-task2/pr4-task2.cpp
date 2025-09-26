e5f4eb39afa6420f3e0fda91ee64b1254a978002
OlegBon <oleh.bon@yahoo.com>
2025-09-26 13:28:28 +0300

    add pr4-task2 and pr4-task3 for WebDev

---
+++ DistSysTech/pr4/pr4-task2/pr4-task2.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main()
+{
+#ifdef _OPENMP
+    printf("OpenMP version: %d\n", _OPENMP);
+#else
+    printf("OpenMP is not supported by this compiler.\n");
+#endif
+    return 0;
+}
---
+++ DistSysTech/pr4/pr4-task3/pr4-task3.cpp
@@ -0,0 +1,43 @@
+﻿// Тут omp_get_num_threads() викликається вже після завершення паралельної області,
+// тобто в послідовному коді, де працює лише головний потік.
+// Тому результат завжди буде 1, незалежно від того, скільки потоків було в паралельному блоці.
+
+//#include <omp.h>
+//#include <stdio.h>
+//
+//int main()
+//{
+//    omp_set_num_threads(8); // Встановлюємо бажану кількість потоків
+//
+//#pragma omp parallel
+//    {
+//        printf("Thread %d is executing\n", omp_get_thread_num());
+//    }
+//    printf("\nTotal threads: %d\n", omp_get_num_threads());
+//
+//    return 0;
+//}
+
+// А тут omp_get_num_threads() викликається всередині паралельної області,
+// і ми обмежуємо вивід лише потоком 0, щоб уникнути дублювання.
+// Це правильний спосіб дізнатися кількість активних потоків.
+
+#include <omp.h>
+#include <stdio.h>
+
+int main()
+{
+    omp_set_num_threads(8); // Встановлюємо бажану кількість потоків
+
+#pragma omp parallel
+    {
+        printf("Thread %d is executing\n", omp_get_thread_num());
+
+        // Вивід кількості потоків зсередини паралельної області
+        if (omp_get_thread_num() == 0) {
+            printf("Total threads: %d\n", omp_get_num_threads());
+        }
+    }
+
+    return 0;
+}
