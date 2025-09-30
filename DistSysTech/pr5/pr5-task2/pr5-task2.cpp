791a72ea8a2df92851fae06c43a87997ac718a4e
OlegBon <oleh.bon@yahoo.com>
2025-09-30 10:42:38 +0300

    add pr5-task2 DistSysTech

---
+++ DistSysTech/pr5/pr5-task2/pr5-task2.cpp
@@ -0,0 +1,21 @@
+﻿#include <omp.h>
+#include <stdio.h>
+
+int main()
+{
+    int n = 20; // Кількість ітерацій
+    int i;
+
+    omp_set_num_threads(4); // Встановлюємо 4 потоки
+
+#pragma omp parallel shared(n) private(i)
+    {
+#pragma omp for
+        for (i = 0; i < n; i++)
+        {
+            printf("Thread %d processes iteration %d\n", omp_get_thread_num(), i);
+        }
+    }
+
+    return 0;
+}
