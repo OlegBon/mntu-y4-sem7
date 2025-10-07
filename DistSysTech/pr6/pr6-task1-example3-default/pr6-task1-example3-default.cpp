16e53a83cf6407bcf85aae7213061ca5f75e5a37
OlegBon <oleh.bon@yahoo.com>
2025-10-07 16:09:27 +0300

    add pr6-task1-example3-default DistSysTech

---
+++ DistSysTech/pr6/pr6-task1-example3-default/pr6-task1-example3-default.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int n = 6; // Кількість ітерацій
+    int i, a, b, c; // Лічильник і змінні
+
+#pragma omp parallel default(shared) private(a, b, c)
+    {
+#pragma omp for
+        for (i = 0; i < n; i++) {
+            a = i + 1;
+            b = i * 2;
+            c = a + b;
+            printf("Thread %d: a = %d, b = %d, c = %d, i = %d\n",
+                omp_get_thread_num(), a, b, c, i);
+        }
+    }
+
+    return 0;
+}
