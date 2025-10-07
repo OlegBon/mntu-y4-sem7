9f7f52aba9dcc7933c7fff81c8349a8c57c5eab1
OlegBon <oleh.bon@yahoo.com>
2025-10-07 11:54:29 +0300

    add pr6-task1-example2-shared DistSysTech

---
+++ DistSysTech/pr6/pr6-task1-example2-shared/pr6-task1-example2-shared.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <omp.h>
+
+int main() {
+	int n = 6; // Кількість ітерацій
+	int i, a[6] = { 0 }; // Лічильник ітерацій та масив для збереження результатів
+#pragma omp parallel for shared(a)
+	for (i = 0; i < n; i++)
+	{
+		a[i] += i;
+		printf("Thread %d has a value of a[%d] = %d for i = %d\n",
+			omp_get_thread_num(), i, a[i], i);
+	}
+}
