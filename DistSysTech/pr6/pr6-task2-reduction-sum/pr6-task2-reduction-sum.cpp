af49c016fe269345e10a5b24f15b123932954cf2
OlegBon <oleh.bon@yahoo.com>
2025-10-07 17:52:14 +0300

    add pr6-task2-reduction-sum DistSysTech

---
+++ DistSysTech/pr6/pr6-task2-reduction-sum/pr6-task2-reduction-sum.cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int i, n = 100;
+    float a[100], sum = 0.0;
+
+    // Ініціалізація масиву
+    for (i = 0; i < n; i++) {
+        a[i] = i + 1.0;
+    }
+
+    // Паралельне обчислення суми з REDUCTION
+#pragma omp parallel for reduction(+:sum)
+    for (i = 0; i < n; i++) {
+        sum += a[i];
+    }
+
+    // Вивід результату
+    printf("Max threads available: %d\n", omp_get_max_threads());
+    printf("Sum of array elements = %f\n", sum);
+
+    return 0;
+}
