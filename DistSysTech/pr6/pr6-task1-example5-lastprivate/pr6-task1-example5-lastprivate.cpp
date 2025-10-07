42087f0e6a5a3c565d8f8744abf14261e0160fb6
OlegBon <oleh.bon@yahoo.com>
2025-10-07 17:11:48 +0300

    add pr6-task1-example5-lastprivat DistSysTech

---
+++ DistSysTech/pr6/pr6-task1-example5-lastprivate/pr6-task1-example5-lastprivate.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int n = 6;
+    int i, a;
+
+#pragma omp parallel for private(i) lastprivate(a)
+    for (i = 0; i < n; i++) {
+        a = i + 1;
+        printf("Thread %d has a value of a = %d for i = %d\n",
+            omp_get_thread_num(), a, i);
+    }
+
+    // Значення змінної a після завершення паралельного циклу
+    printf("Value of a after parallel for: a = %d\n", a);
+
+    return 0;
+}
