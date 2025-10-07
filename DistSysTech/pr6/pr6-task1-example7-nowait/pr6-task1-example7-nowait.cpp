f5bfc708a96e76975f5bbb4700dc674858716d53
OlegBon <oleh.bon@yahoo.com>
2025-10-07 17:35:45 +0300

    add pr6-task1-example7-nowait DistSysTech

---
+++ DistSysTech/pr6/pr6-task1-example7-nowait/pr6-task1-example7-nowait.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int i, n = 6;
+
+#pragma omp parallel
+    {
+        // Перша частина — цикл з nowait
+#pragma omp for nowait
+        for (i = 0; i < n; i++) {
+            printf("Thread %d processing i = %d\n", omp_get_thread_num(), i);
+        }
+
+        // Друга частина — виконується одразу після циклу, без очікування інших потоків
+        printf("Thread %d continues without waiting\n", omp_get_thread_num());
+    }
+
+    return 0;
+}
